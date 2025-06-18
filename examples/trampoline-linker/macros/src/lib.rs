e56bc12a6c25f9f7118dca8b51e46fce000fb728
Ludovic Mermod <ludovic.mermod@epfl.ch>
2025-06-18 11:44:33 +0200

    example: add trampoline linker example

---
+++ examples/trampoline-linker/macros/src/lib.rs
@@ -0,0 +1,55 @@
+use core::panic;
+
+use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::Item;
+
+#[proc_macro_attribute]
+pub fn hook(_attr: TokenStream, mut item: TokenStream) -> TokenStream {
+    let Ok(Item::Fn(fun)) = syn::parse::<Item>(item.clone()) else {
+        panic!("Macro can only be used on function");
+    };
+
+    let ident = fun.sig.ident;
+    let trampoline_ident = format_ident!("__{}_trampoline", ident);
+
+    let trampoline: TokenStream = quote! {
+        fn #trampoline_ident() {
+            unsafe {
+                ::core::arch::asm!(
+                    // Save the resolved address of the symbol in the stack. The actual value written must be changed by the linker.
+                    "mov rax,{}",
+                    "mov [rsp],rax",
+                    // Stores all the registers potentially containing arguments on the stack. All other temporary registers are not
+                    // used across the call by the trampoline and thus do not need to be saved.
+                    "push rcx",
+                    "push rdx",
+                    "push rsi",
+                    "push rdi",
+                    "push r8",
+                    "push r9",
+                    // Call the hook
+                    "call {}",
+                    // Pops the arguments back into the corresponding registers.
+                    "pop r9",
+                    "pop r8",
+                    "pop rdi",
+                    "pop rsi",
+                    "pop rdx",
+                    "pop rcx",
+                    // Recovers the actual symbol to jump to, and jump there will passing the return address of the current function
+                    // frame to the callee.
+                    "pop rbx",
+                    "mov rax,[rsp]",
+                    "jmp rbx",
+                    const 0x55555555ba70i64,
+                    sym #ident
+                );
+            }
+        }
+    }
+    .into();
+
+    item.extend(trampoline);
+    item
+}
---
+++ examples/trampoline-linker/src/main.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+extern crate fold;
+
+extern crate macros;
+
+pub mod installer;
+
+use core::ffi::CStr;
+
+use fold::{
+    Env, Exit, exit,
+    filters::{self, ObjectFilter},
+    init_logging, println,
+};
+use macros::hook;
+
+use crate::installer::TrampolineReloc;
+
+#[hook]
+fn foo(str: *const i8) {
+    println!(
+        "[from hook]: puts called with \"{}\" !",
+        unsafe { CStr::from_ptr(str) }.to_string_lossy()
+    )
+}
+
+fold::entry!(entry);
+
+fn entry(env: Env) -> ! {
+    init_logging(log::LevelFilter::Trace);
+
+    fold::default_chain(env)
+        .insert_phase_after("hooks", "relocation")
+        .register_in_phase(
+            "hooks",
+            TrampolineReloc::new().with_hook("puts", __foo_trampoline),
+            ObjectFilter {
+                mask: filters::ObjectMask::Any,
+                os_abi: 0,
+                elf_type: 0,
+            },
+        )
+        .run();
+
+    exit(Exit::Success);
+}
